abd005178be9b20c3c68759bb120167e96405685
Azero 77 <zaggar.azero.m@gmail.com>
2025-07-26 14:52:15 +0300

    Added Repository diagram

---
+++ src/ClinicApp.Infrastructure/Extensions/InfrastructureMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace ClinicApp.Infrastructure.Extensions;
+
+using ClinicApp.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+public static class InfrastructureMiddlewareExtensions
+{
+    public static IApplicationBuilder  UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<EventualConsistencyMiddleware>();
+        return app;
+    }
+}
+
