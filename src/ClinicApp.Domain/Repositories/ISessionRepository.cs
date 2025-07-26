cd9e25ef473196399eb5772c0281cc419765ded3
Azero 77 <zaggar.azero.m@gmail.com>
2025-07-26 16:13:58 +0300

    Added Session Repository Interface

--- src/ClinicApp.Domain/Repositories/ISessionRepository.cs
+++ src/ClinicApp.Domain/Repositories/ISessionRepository.cs
@@ -7,5 +7,15 @@ public interface ISessionRepository
 {
     Task<IEnumerable<Session.Session>> GetFutureSessionsDoctor(Doctor.Doctor doctor);
     Task<IEnumerable<Session.Session>> GetAllSessionsForDoctor(Doctor.Doctor doctor);
+    Task<ErrorOr<Session.Session>> GetSessionById(Guid sessionId);
+    Task<IEnumerable<Session.Session>> GetSessionsForToday();
+    Task<IEnumerable<Session.Session>> GetSessionsForDoctorToday(Guid doctorid);
+    Task<IEnumerable<Session.Session>> GetSessionsForDay(DateOnly date);
+    Task<ErrorOr<Created>> AddSession(Session.Session session);
+    Task<ErrorOr<Success>> SetSession(Session.Session session);
+    Task<ErrorOr<Updated>> UpdateSession(Session.Session session);
+    Task<ErrorOr<Success>> RejectSession(Session.Session session);
+    Task<ErrorOr<Deleted>> DeleteSession(Guid sessionId);
+    Task<IEnumerable<SessionState>> GetSessionHistory(Guid sessionId);
     Task RemoveSessionFromDoctor(Session.Session session);
 }
