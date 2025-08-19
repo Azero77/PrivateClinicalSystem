4096a406b2f464c6d78a86041f0365a133881b1a
Azero 77 <zaggar.azero.m@gmail.com>
2025-08-19 21:57:27 +0300

    Added Filtering Sorting

--- src/ClinicApp.Infrastructure/Repositories/IPaginatedRepository.cs
+++ src/ClinicApp.Infrastructure/Repositories/IPaginatedRepository.cs
@@ -1,7 +1,9 @@
 ﻿using ClinicApp.Domain.Common;
-using ClinicApp.Domain.DoctorAgg;
 using ClinicApp.Infrastructure.Persistance;
+using ErrorOr;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace ClinicApp.Infrastructure.Repositories;
 public interface IPaginatedRepository<T>
@@ -9,6 +11,7 @@ public interface IPaginatedRepository<T>
 {
     Task<IReadOnlyCollection<T>> GetAll(int pageNumber, int itemsPerPage);
     Task<IReadOnlyCollection<T>> GetAll(int pageNumber);
+    Task<ErrorOr<IReadOnlyCollection<T>>> GetItems(DataQueryOptions<T> opts);
 }
 
 public class PaginatedRepostiory<T> : IPaginatedRepository<T>
@@ -32,6 +35,56 @@ public class PaginatedRepostiory<T> : IPaginatedRepository<T>
 
     public Task<IReadOnlyCollection<T>> GetAll(int pageNumber)
     {
-        return GetAll(pageNumber,ItemsPerPage);
+        return GetAll(pageNumber, ItemsPerPage);
     }
+
+    public async Task<ErrorOr<IReadOnlyCollection<T>>> GetItems(DataQueryOptions<T> opts)
+    {
+        var query = _context.Set<T>().AsNoTracking();
+
+        if (opts.filtering is not null)
+        {
+            query = query.Where(opts.filtering);
+        }
+
+        if (opts.orderbyItem is not null && opts.orderSchema is not null)
+        {
+            PropertyInfo? prop = typeof(T)
+                .GetProperty(opts.orderbyItem, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if (prop is null)
+                return Error.Validation("Infrastructure.Validation", $"No property of name {opts.orderbyItem} in {typeof(T).Name}");
+            if (!Enum.TryParse<OrderSchema>(opts.orderSchema, true, out OrderSchema orderSchema))
+                return Error.Validation("Infrastructure.Validation", "No Proper OrderSchema is configured");
+
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var property = Expression.Property(parameter, prop.Name);
+            var keySelector = Expression.Lambda(property, parameter);
+
+            var methodName = orderSchema == OrderSchema.Asc ? "OrderBy" : "OrderByDescending";
+
+            var orderByCall = Expression.Call(
+                typeof(Queryable),
+                methodName,
+                new[] { typeof(T), prop.PropertyType },
+                query.Expression,
+                Expression.Quote(keySelector));
+
+            query = query.Provider.CreateQuery<T>(orderByCall);
+        }
+
+        return await query
+            .Skip(ItemsPerPage * opts.pageNumber)
+            .Take(ItemsPerPage)
+            .ToListAsync();
+    }
+
+
+    enum OrderSchema
+    {
+        Asc,
+        Desc
+    }
+
 }
+
+public record DataQueryOptions<T>(int pageNumber, string? orderbyItem = null, string? orderSchema = null, Expression<Func<T, bool>>? filtering = null);
