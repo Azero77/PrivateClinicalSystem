a7f5d03fd828b557c0be5ee9025a8dda54323dc6
Azero 77 <zaggar.azero.m@gmail.com>
2025-08-24 17:30:25 +0300

    Added Projects without implementation

---
+++ src/ClinicApp.Identity.Server/Config.cs
@@ -0,0 +1,19 @@
+using Duende.IdentityServer.Models;
+
+namespace ClinicApp.Identity.Server;
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            { };
+}
