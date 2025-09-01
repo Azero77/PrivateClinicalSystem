4b3ec7d7b37c0e4d6d30da01eeb5ce8cdfd4860c
Azero 77 <zaggar.azero.m@gmail.com>
2025-09-01 15:09:22 +0300

    Configured The renaming projects problem

--- src/ClinicApp.Domain/Common/Interfaces/IClock.cs
+++ src/ClinicApp.Domain/Common/Interfaces/IClock.cs
@@ -2,5 +2,5 @@
 
 public interface IClock
 {
-    public DateTimeOffset UtcNow => DateTime.UtcNow;
+    public DateTime UtcNow => DateTime.UtcNow;
 }
--- tests/ClinicApp.Domain.Tests.UnitTest/Factories.cs
+++ tests/ClinicApp.Domain.Tests.UnitTest/Factories.cs
@@ -12,7 +12,7 @@ namespace ClinicApp.Domain.Tests.UnitTest
     {
         public static Doctor DoctorFactory => new Doctor(Guid.NewGuid(),
                 WorkingDays.Monday | WorkingDays.Wednesday,
-                WorkingHours.Create(new TimeOnly(9, 0), new TimeOnly(17, 0)),
+                WorkingHours.Create(new TimeOnly(9, 0), new TimeOnly(17, 0)).Value,
                 Guid.NewGuid(),
                 major : null);
     }
