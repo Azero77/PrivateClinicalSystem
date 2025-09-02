b948ee9cfbc6931a60213fd8d199a92c64129251
Azero 77 <zaggar.azero.m@gmail.com>
2025-09-02 14:26:15 +0300

    Added DataModels

---
+++ src/ClinicApp.Infrastructure/Persistance/DataModels/DoctorDataModel.cs
@@ -0,0 +1,16 @@
+﻿using ClinicApp.Domain.DoctorAgg;
+
+namespace ClinicApp.Infrastructure.Persistance.DataModels;
+public class DoctorDataModel
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public WorkingTime WorkingTime { get; set; } = null!;
+    public string? Major { get; set; }
+    public Guid RoomId { get; set; }
+    public RoomDataModel Room { get; set; } = null!;
+
+    public IReadOnlyCollection<SessionDataModel> Sessions = new List<SessionDataModel>();
+}
---
+++ src/ClinicApp.Infrastructure/Persistance/DataModels/RoomDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicApp.Infrastructure.Persistance.DataModels;
+
+public class RoomDataModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DoctorDataModel Docotor { get; set; } = null!;
+
+}
